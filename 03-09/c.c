2322a7afefd1d57484313b73c930c7f078effc1a
b_h14@naver.com <b_h14@naver.com>
2023-03-10 08:25:58 +0900

    초기 커밋.

---
+++ 03-09/c.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	int sum = 0;
+
+	for (int i = 2; i < 101; i++) {
+		for (int i2 = 2; i2 <= i; i2++) {
+			if (i != i2 && i % i2 == 0)
+				break;
+			else if (i == i2) {
+				sum += i;
+				printf("%d\t", i);
+			}
+		}
+	}
+	printf("\n소수의 합은 %d 이지요오오\n", sum);
+}
